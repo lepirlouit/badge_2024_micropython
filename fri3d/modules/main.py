da806202eec370f574c79799fecfbabdc28c3f82
Mathy Vanvoorden <mathy@vanvoorden.be>
2024-05-16 13:52:08 +0200

    Auto-boot REPL when SAO gpio2 is pulled down

--- fri3d/modules/main.py
+++ fri3d/modules/main.py
@@ -1,7 +1,9 @@
+import machine
 import time
 import lvgl_esp32
 import lvgl as lv
 
+from fri3d.badge.hardware import hardware_pinout
 from fri3d.badge import leds, display
 
 
@@ -43,32 +45,41 @@ def demo(np):
     np.write()
 
 
-print("Boot complete, starting application")
-demo(leds)
-
+# Initialize the screen
 wrapper = lvgl_esp32.Wrapper(display)
 wrapper.init()
 
-screen = lv.screen_active()
-screen.set_style_bg_color(lv.color_hex(0x003a57), lv.PART.MAIN)
+# We check some inputs at boot to see if we need to boot in a special mode
+repl_pin = machine.Pin(hardware_pinout.pinout_sao.gpio2, machine.Pin.IN, machine.Pin.PULL_UP)
 
-label = lv.label(screen)
-label.set_text("Hello world from MicroPython")
-label.set_style_text_color(lv.color_hex(0xffffff), lv.PART.MAIN)
-label.align(lv.ALIGN.CENTER, 0, 0)
 
-a = lv.anim_t()
-a.init()
-a.set_var(label)
-a.set_values(10, 50)
-a.set_duration(1000)
-a.set_playback_delay(100)
-a.set_playback_duration(300)
-a.set_repeat_delay(500)
-a.set_repeat_count(lv.ANIM_REPEAT_INFINITE)
-a.set_path_cb(lv.anim_t.path_ease_in_out)
-a.set_custom_exec_cb(lambda _, v: label.set_y(v))
-a.start()
+if repl_pin.value() == 0:
+    print("Detected REPL pin active, dropping into REPL")
 
-while True:
-    lv.timer_handler_run_in_period(5)
+else:
+    print("Boot complete, starting application")
+    demo(leds)
+
+    screen = lv.screen_active()
+    screen.set_style_bg_color(lv.palette_darken(lv.PALETTE.GREY, 4), lv.PART.MAIN)
+
+    label = lv.label(screen)
+    label.set_text("Hello world from MicroPython")
+    label.set_style_text_color(lv.color_hex(0xffffff), lv.PART.MAIN)
+    label.align(lv.ALIGN.CENTER, 0, 0)
+
+    a = lv.anim_t()
+    a.init()
+    a.set_var(label)
+    a.set_values(10, 50)
+    a.set_duration(1000)
+    a.set_playback_delay(100)
+    a.set_playback_duration(300)
+    a.set_repeat_delay(500)
+    a.set_repeat_count(lv.ANIM_REPEAT_INFINITE)
+    a.set_path_cb(lv.anim_t.path_ease_in_out)
+    a.set_custom_exec_cb(lambda _, v: label.set_y(v))
+    a.start()
+
+    while True:
+        lv.timer_handler_run_in_period(5)
--- ports/esp32/boards/FRI3D_BADGE_2024/modules/fri3d/badge/hardware/pinout.py
+++ ports/esp32/boards/FRI3D_BADGE_2024/modules/fri3d/badge/hardware/pinout.py
@@ -18,10 +18,16 @@ class HardwarePinout:
             self.pin_dc = 4
             self.pin_cs = 5
 
+    class PinoutSAO:
+        def __init__(self, leds: PinoutLEDS):
+            self.gpio1 = leds.pin
+            self.gpio2 = 13
+
     def __init__(self):
         self.pinout_leds = self.PinoutLEDS()
         self.pinout_spi = self.PinoutSPI()
         self.pinout_display = self.PinoutDisplay()
+        self.pinout_sao = self.PinoutSAO(self.pinout_leds)
 
 
 hardware_pinout = HardwarePinout()
