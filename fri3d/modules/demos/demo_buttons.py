e1ae877d759cd262fde04492971f6e170e97dde7
Joram Agten <joram.agten@vito.be>
2024-05-29 01:48:37 +0200

    fix demo_buttons

--- fri3d/modules/demos/demo_buttons.py
+++ fri3d/modules/demos/demo_buttons.py
@@ -1,33 +1,28 @@
 import time
 
-from fri3d.badge import button_a, button_b, button_x, button_y, button_menu, button_start
-from fri3d.badge import joystick_axis_x, joystick_axis_y
+from fri3d.badge import buttons
+from fri3d.badge import joystick
 
 
 if __name__ == "__main__":
 
-    try:
-        c = joystick_axis_x.calibrate_center()
+    if 'x' in joystick and 'y' in joystick:
+        analog_joystick = True
+        c = joystick['x'].calibrate_center()
         print("x calibrate: ", c)
 
-        c = joystick_axis_y.calibrate_center()
+        c = joystick['y'].calibrate_center()
         print("y calibrate: ", c)
 
-        for _ in range(100):
-            j_x = joystick_axis_x.read()
-            j_y = joystick_axis_y.read()
-            a = button_a.value()
-            b = button_b.value()
-            x = button_x.value()
-            y = button_y.value()
-            menu = button_menu.value()
-            start = button_start.value()
+    for _ in range(100):
+        values = {}
+        if analog_joystick:
+            values['j_x'] = joystick['x'].read()
+            values['j_y'] = joystick['y'].read()
+        
+        for b in buttons:
+            values[b] = buttons[b].value()
 
-            print(f"{j_x=}, {j_y=}, {a=}, {b=}, {x=}, {y=}, {menu=}, {start=}")
+        print(f"{values=}")
 
-            time.sleep(0.1)
-
-    except KeyboardInterrupt:
-        joystick_axis_x.deinit()
-        joystick_axis_y.deinit()
-        pass
+        time.sleep(0.1)
