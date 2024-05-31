4f8d7195ff072aab839be551bccf90cdb3771969
Joram Agten <joram.agten@vito.be>
2024-05-31 14:40:38 +0200

    nvs as copied from https://github.com/kdschlosser/lvgl_micropython/tree/4fc7d5cbd9c9ddbb8d73b7317b9bdd155cce4e7d/ext_mod/esp32_additions/nvs

---
+++ fri3d/nvs/README.md
@@ -0,0 +1,4 @@
+# NVS
+as copied from https://github.com/kdschlosser/lvgl_micropython/tree/4fc7d5cbd9c9ddbb8d73b7317b9bdd155cce4e7d/ext_mod/esp32_additions/nvs
+all credits to @kdschlosser
+
--- ports/esp32/boards/FRI3D_BADGE_COMMON/fri3d_lvgl.cmake
+++ ports/esp32/boards/FRI3D_BADGE_COMMON/fri3d_lvgl.cmake
@@ -5,3 +5,8 @@ string(CONCAT GIT_SUBMODULES "${GIT_SUBMODULES} " fri3d/lvgl_esp32_mpy)
 list(APPEND USER_C_MODULES
     ../../../fri3d/lvgl_esp32_mpy/micropython.cmake
 )
+
+# Inject the driver as a user module
+list(APPEND USER_C_MODULES
+    ../../../fri3d/nvs/micropython.cmake
+)
