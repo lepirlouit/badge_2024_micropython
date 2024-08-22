2e868ad1869f91343135abbb04fbb309ef6e853d
LePirlouit <lepirlouit@users.noreply.github.com>
2024-08-22 11:06:11 +0200

    Increase Dead zone
    dead zone seams to be way bigger than expected, this is tested with the joystick.py example
    
    Signed-off-by: LePirlouit <lepirlouit@users.noreply.github.com>

--- fri3d/fri3d_application/src/payload/fri3d/badge/joystick.py
+++ fri3d/fri3d_application/src/payload/fri3d/badge/joystick.py
@@ -10,10 +10,10 @@ class Joystick:
         if hardware_capabilities.joystick:
             from fri3d.badge.hardware import hardware_joystick
 
-            self.x = JoystickAxis(hardware_joystick.pinout.pin_joystick_x, dead_val=150)
+            self.x = JoystickAxis(hardware_joystick.pinout.pin_joystick_x, dead_val=400)
             self.x.init()
 
-            self.y = JoystickAxis(hardware_joystick.pinout.pin_joystick_y, dead_val=150)
+            self.y = JoystickAxis(hardware_joystick.pinout.pin_joystick_y, dead_val=400)
             self.y.init()
 
 
